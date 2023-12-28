a324732c76c9df166bf408572aa5d847c7bc5e81
Vinicius G. Saggioratto <viniciussaggioratto@gmail.com>
2023-12-28 16:45:37 -0300

    Configuração inicial do projeto Rural App.

---
+++ src/main/java/com/vgs/rural/RuralApplication.java
@@ -0,0 +1,13 @@
+package com.vgs.rural;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuralApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vgs/rural/RuralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vgs.rural;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
