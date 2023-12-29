e023a9ee26034947563a096e7897d86d53089a7d
Vinicius G. Saggioratto <viniciussaggioratto@gmail.com>
2023-12-29 16:11:44 -0300

    Criado novo enum typebuilding.

---
+++ src/main/java/com/vgs/rural/enums/TypeBuilding.java
@@ -0,0 +1,13 @@
+package com.vgs.rural.enums;
+
+public enum TypeBuilding {
+    GERAL,
+    PAVILHAO,
+    GALPAO,
+    ARMAZEM,
+    SILO,
+    AVIARIO,
+    ACUDE,
+    AREA_TERRA,
+    CHIQUEIRO
+}
